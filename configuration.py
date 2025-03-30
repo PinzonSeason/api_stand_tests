7f12358122e986b0c8e92f24a0a58ff8ad24af28
Eduardo Reyna Hernandez <n6eduardo@gmail.com>
2025-03-29 19:15:17 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-1f0d80c7-8412-43ed-8b40-40ba8842f7ac.containerhub.tripleten-services.com"
+URL_SERVICE = ""
 DOC_PATH = "/docs/"
 LOG_MAIN_PATH = "/api/logs/main/"
 USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
